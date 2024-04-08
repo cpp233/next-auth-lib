0c06997611fa2b0e87a033ae754aa0aecbf85582
cpp233 <tincint@foxmail.com>
2024-04-08 22:19:06 +0800

    'init'

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <>
+      <Button variant='primary'>按钮</Button>
+      <p className='font-semibold'>init</p>
+    </>
+  );
+}
