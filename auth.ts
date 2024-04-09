03561604b2e0caa08607c1869a0529cbe71d8045
cpp233 <tincint@foxmail.com>
2024-04-09 20:37:08 +0800

    next-auth 修改字段

--- auth.ts
+++ auth.ts
@@ -3,6 +3,8 @@ import NextAuth from 'next-auth';
 import authConfig from '@/auth.config';
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import { db } from '@/lib/db';
+import { getUserById } from './data/user';
+import { UserRole } from '@prisma/client';
 
 export const {
   handlers: { GET, POST },
@@ -10,6 +12,50 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  callbacks: {
+    async signIn({ user }) {
+      // console.log('[signIn]', user);
+      return true;
+      if (!user.id) {
+        return false;
+      }
+
+      const existingUser = await getUserById(user.id);
+      if (!existingUser || !existingUser?.emailVerified) {
+        return false;
+      }
+      return true;
+    },
+    async session({ session, token }) {
+      // console.log('[session]', session, token);
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.sub && session.user && token.role) {
+        session.user.role = token.role;
+      }
+
+      return session;
+    },
+    async jwt({ token }) {
+      // console.log('[jwt]', token);
+      // token.addFiled = 'new token field';
+
+      if (!token.sub) {
+        return token;
+      }
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) {
+        return token;
+      }
+
+      token.role = existingUser.role;
+      return token;
+    },
+  },
   // 兼容 prisma 的 edge
   adapter: PrismaAdapter(db),
   session: { strategy: 'jwt' }, // 使用 jwt 作为 session 策略
---
+++ next-auth.d.ts
@@ -0,0 +1,22 @@
+// https://authjs.dev/getting-started/typescript
+import { UserRole } from '@prisma/client';
+import type { DefaultSession } from 'next-auth';
+export type ExtendUser = DefaultSession['user'] & {
+  role: UserRole;
+};
+declare module 'next-auth' {
+  interface Session {
+    user: ExtendUser;
+  }
+}
+
+import type { JWT } from 'next-auth/jwt';
+export type ExtendJWT = JWT & {
+  token: ExtendUser;
+};
+declare module 'next-auth/jwt' {
+  interface JWT {
+    role?: UserRole;
+    // fish: string;
+  }
+}
